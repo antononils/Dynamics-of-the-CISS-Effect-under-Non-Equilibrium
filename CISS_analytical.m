3166f15dfb78c2bf47364dfc3f4bb346346b7116
christoffer100 <christoffer@damsgaardfalck.se>
2024-05-07 12:33:57 +0200

    Uppdate

--- CISS_analytical.m
+++ CISS_analytical.m
@@ -7,24 +7,25 @@ c = 30e-10;
 lambda = 1e-3;
 Gamma = 50e-3;
 T = 10;
-time_step = 5;
+time_step = 50;
 p = [0 0 0];
 chrility = true;
-perturbation = None;
+perturbation = [];
 
 
 
 % f = -1i*50*cos(10*t); 
 func = 0;
 
-H_pos = Perturbation_analytic("pos",a,c,lambda,chrility,perturbation,p,func);
-H_neg = Perturbation_analytic("neg",a,c,lambda,chrility,perturbation,p,func);
+H_pos = Perturbation_analytic("pos",a,c,lambda, Gamma, chrility, perturbation,p,func);
+H_neg = Perturbation_analytic("neg", a, c, lambda, Gamma, chrility, perturbation, p, func);
+H_neg
+[J_pos_up, J_pos_down, t_vec] = Probablity_density(H_pos, eps, y, T, time_step);
+[J_neg_up, J_neg_down, t_vec]= Probablity_density(H_neg, eps, y, T, time_step);
 
-J_pos = Probablity_density(H_pos, T, time_step);
-J_neg = Probablity_density(H_neg, T, time_step);
 
 Xdata = [1 2 3 4];
-ColorPlot(Xdata, J_neg(3), J_pos(1)-J_pos(2), J_neg(1)-J_neg(2), "Probability density", "Spin", "Site", "Time","Alternative")
+ColorPlot(Xdata, t_vec, J_pos_up-J_pos_down, J_neg_up-J_neg_down, "Probability density", "Spin", "Site", "Time","Alternative")
 
 
 
--- Perturbation_analytic.m
+++ Perturbation_analytic.m
@@ -1,4 +1,4 @@
-function H = Perturbation_analytic(Helicity, radius, length,lambda, chirility, perturbation,p,func)
+function H = Perturbation_analytic(Helicity, radius, length, lambda, Gamma, chirility, perturbation,p,func)
     a = radius;
     c = length;
     d= 2*a*c*(1/3);
@@ -20,7 +20,7 @@ function H = Perturbation_analytic(Helicity, radius, length,lambda, chirility, p
         h_42 = g*[-1i*b -d;
             d  1i*b];
             
-        H = [h_11 zeros(2,2) h_13 zeros(2,2);
+        H = [zeros(2,2) zeros(2,2) h_13 zeros(2,2);
             zeros(2,2) zeros(2,2) zeros(2,2) h_24;
             h_31 zeros(2,2) zeros(2,2) zeros(2,2);
             zeros(2,2) h_42 zeros(2,2) zeros(2,2)];
@@ -34,30 +34,33 @@ function H = Perturbation_analytic(Helicity, radius, length,lambda, chirility, p
         h_42 = g*[1i*b -d;
             d  -1i*b];
             
-        H = lambda*[h_11 zeros(2,2) h_13 zeros(2,2);
+        H = lambda*[zeros(2,2) zeros(2,2) h_13 zeros(2,2);
             zeros(2,2) zeros(2,2) zeros(2,2) h_24;
             h_31 zeros(2,2) zeros(2,2) zeros(2,2);
             zeros(2,2) h_42 zeros(2,2) zeros(2,2)];
     end
 
     V = zeros(8,8);
+    if isempty(perturbation)
+     
+    else
+        if  ismember("metal_r", perturbation)
+            v = Gamma*(eye(2,2)+[p(3) p(1)-1i*p(2);
+                p(1)+1i*p(2) -p(3)]);
+            V = V + [zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) v];
+        end
 
-    if  ismember("metal_r", perturbation)
-        v = Gammma*(eye(2,2)+[p(3) p(1)-1i*p(2);
-            p(1)+1i*p(2) -p(3)]);
-        V = V + [zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) v];
-    end
-
-    if  ismember("metal_l", perturbation)
-        v = Gammma*(eye(2,2)+[p(3) p(1)-1i*p(2);
-            p(1)+1i*p(2) -p(3)]);
-        V = V + [v zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
-            zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2)];
+        if  ismember("metal_l", perturbation)
+            v = Gamma*(eye(2,2)+[p(3) p(1)-1i*p(2);
+                p(1)+1i*p(2) -p(3)]);
+            V = V + [v zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2);
+                zeros(2,2) zeros(2,2) zeros(2,2) zeros(2,2)];
+        end
     end
 
     H = H + func*V;
